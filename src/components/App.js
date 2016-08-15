5e9b7454716a61712aee0c2a6ac31cf39fdbdd64
duhdanube <Danny.B.Lim@gmail.com>
2016-08-15 09:45:24 -0700

    Setup complete

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = React.createClass({
+  render() {
+    return (
+      <h1>Product Organizer</h1>
+
+    )
+  }
+})
+
+export default App;
